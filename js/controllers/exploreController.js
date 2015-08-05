11ff940ce7dbc3c0019c4140cd54ca9238473c44
Michele Minno <minno@cybion.it>
2015-08-05 18:40:39 +0200

    Working on explore page

--- js/controllers/exploreController.js
+++ js/controllers/exploreController.js
@@ -1,4 +1,30 @@
-var exploreController = controllers.controller("ExploreCtrl", function($scope) {
+var exploreController = controllers.controller("ExploreCtrl", function($scope,
+		MyFamilyService, AuthenticationService) {
 
 	d3.select("svg").attr("opacity", .1);
+
+	$scope.search = function() {
+
+		MyFamilyService.getGraphView(AuthenticationService.getUserId(), 4)
+				.then(
+						function(resultData) {
+
+							var data = {
+								nodes : resultData.nodes
+							};
+
+							MyFamilyService.getViewDocuments(
+									AuthenticationService.getUserId(), data)
+									.then(function(resultData) {
+
+										$scope.results = resultData;
+										$scope.documentsReady = true;
+									});
+						});
+	};
+
+	$scope.getFilePath = function(docName) {
+
+		return MyFamilyService.getFilePath(docName);
+	};
 });
