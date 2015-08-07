844387842f8c131ce0481a805b0097e1d2839578
Michele Minno <minno@cybion.it>
2015-08-07 17:14:32 +0200

    DB schema updated

--- myfamily.sql
+++ myfamily.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Aug 03, 2015 at 10:53 AM
+-- Generation Time: Aug 07, 2015 at 05:14 PM
 -- Server version: 5.5.29
 -- PHP Version: 5.3.28
 
@@ -37,6 +37,18 @@ CREATE TABLE IF NOT EXISTS `blacklist` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `bookmarks`
+--
+
+DROP TABLE IF EXISTS `bookmarks`;
+CREATE TABLE IF NOT EXISTS `bookmarks` (
+  `user` int(11) NOT NULL,
+  `document` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `documents`
 --
 
@@ -48,7 +60,7 @@ CREATE TABLE IF NOT EXISTS `documents` (
   `file` varchar(100) NOT NULL,
   `owner` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=33 ;
 
 -- --------------------------------------------------------
 
@@ -64,8 +76,9 @@ CREATE TABLE IF NOT EXISTS `events` (
   `entity_type` varchar(15) NOT NULL,
   `node` int(11) NOT NULL,
   `date` datetime NOT NULL,
+  `user` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=29 ;
 
 -- --------------------------------------------------------
 
@@ -93,7 +106,7 @@ CREATE TABLE IF NOT EXISTS `nodes` (
   `img` varchar(50) NOT NULL,
   `person` tinyint(1) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=26 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=33 ;
 
 -- --------------------------------------------------------
 
@@ -138,7 +151,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   `credentials` varchar(50) NOT NULL,
   `node` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=63 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=65 ;
 
 -- --------------------------------------------------------
 
