b9bf598a7c5fedf40c0adeb5fa23012cc3186031
Michele Minno <minno@cybion.it>
2015-10-09 15:59:25 +0200

    Wrapping drawer labels

--- js/graph-render.js
+++ js/graph-render.js
@@ -103,7 +103,7 @@ var graphRender = function(scope, graph, configuration, server, svg) {
 				function(drawers) {
 
 					scope.graph.drawers = drawers;
-
+				
 					var offset = 0;
 
 					svg.drawer = svg.drawer.data(drawers).enter().append("g");
@@ -117,12 +117,12 @@ var graphRender = function(scope, graph, configuration, server, svg) {
 										drawerClicked).on('contextmenu',
 										d3.contextMenu(onDrawerMenu));
 
-						var xVal = -140 + offset;
+						var xVal = -145 + offset;
 						var yVal = 1270;
 
 						d3.select(this).append("text").attr('class',
 								"nodeLabel").attr("y", yVal).attr("x", xVal)
-								.text(d.label).attr(
+								.text(d.label).call(wrap, 150).attr(
 										"transform",
 										function(d) {
 
@@ -1784,5 +1784,29 @@ var graphRender = function(scope, graph, configuration, server, svg) {
 		var y = document.getAttribute("y");
 
 		server.updateDocumentPosition(d.id, graph.selectedNode.id, x, y);
+	}
+
+	function wrap(text, width) {
+		text
+				.each(function() {
+					var text = d3.select(this), words = text.text().split(/\s+/)
+							.reverse(), word, line = [], lineNumber = 0, lineHeight = 1.1, // ems
+					x = text.attr("x"), y = text.attr("y"), dy = 0, // parseFloat(text.attr("dy")),
+					tspan = text.text(null).append("tspan").attr("x", x).attr(
+							"y", y).attr("dy", dy + "em");
+					while (word = words.pop()) {
+						line.push(word);
+						tspan.text(line.join(" "));
+						if (tspan.node().getComputedTextLength() > width) {
+							line.pop();
+							tspan.text(line.join(" "));
+							line = [ word ];
+							tspan = text.append("tspan").attr("x", x).attr("y",
+									y).attr("dy",
+									++lineNumber * lineHeight + dy + "em")
+									.text(word);
+						}
+					}
+				});
 	}
 };
